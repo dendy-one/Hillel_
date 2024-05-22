1afcebe41f17ba5094d5d463513b047e9eb67f45
dendy-one <danya.homon0202@gmail.com>
2024-05-22 17:27:13 +0200

    Добавьте файлы проекта.

---
+++ Task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, number3, number4;
+            Console.WriteLine("Enter the first number");
+            number1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number");
+            number3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the fourth number");
+            number4 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"{(number1 * 1000)+(number2*100)+(number3*10)+number4}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int number2 = 0;
+            while (number != 0)
+            {
+                number2 = number2 * 10 + number % 10;
+                number /= 10;
+            }
+            Console.WriteLine(number2);
+            Console.ReadKey();
+
+
+            
+        }
+
+    }
+}
