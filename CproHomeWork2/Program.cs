a0f374f7e5d5e6a9ded145c43aefefee9e3debe5
dendy-one <150953486+dendy-one@users.noreply.github.com>
2024-08-17 23:35:28 +0200

    Add files via upload

---
+++ CproHomeWork2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+        Circle circle = new Circle(0, 0, 5);
+        circle.Move(10, 15);
+
+
+        Rectangle rectangle = new Rectangle(0, 0, 20, 10);
+        rectangle.Move(5, 5);
+
+
+        Vector resizeVector = new Vector(10, 20);
+        rectangle.Resize(resizeVector);
+
+
+        Console.WriteLine($"Довжина вектора: {resizeVector.Length()}");
+        Console.ReadKey();
+    }
+}
